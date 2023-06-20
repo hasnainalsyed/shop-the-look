532c39803110a0f7f33783bba8ad1e5538a23aa1
Hasnain Syed <hasnainalsyed@gmail.com>
2023-06-20 20:17:59 +0500

    Added Dot functions

---
+++ backup.js
@@ -0,0 +1,88 @@
+// Initialize Swiper with vertical slider
+var swiper = new Swiper('.main-swiper-container', {
+  slidesPerView: 1, // Set the number of visible slides to 1
+  spaceBetween: 10, // Adjust the spacing between slides
+  // Configuration options for the main slider
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+
+// var innerSwipers = document.querySelectorAll('.inner-swiper-container');
+// innerSwipers.forEach(function (innerSwiperContainer) {
+//   console.log(innerSwiperContainer);
+//   var innerSwiper = new Swiper(innerSwiperContainer, {
+//     direction: 'vertical',
+//     // other configuration options for the inner swiper
+//     spaceBetween: 10, // Adjust the spacing between slides
+//     slidesPerView: 1, // Set the number of visible slides to 1
+//     mousewheel: {
+//       enabled: true
+//     },
+//     on: {
+//       slideChange: function () {
+//         var activeIndex = this.activeIndex;
+//         var innerSwiperContainerParent = innerSwiperContainer.parentElement.parentElement;
+//         var dotButtons = innerSwiperContainerParent.querySelectorAll('.btn-dot');
+
+//         dotButtons.forEach(function (button, index) {
+//           console.log(button, index);
+//           if (index === activeIndex) {
+//             button.classList.add('active');
+//           } else {
+//             button.classList.remove('active');
+//           }
+//         });
+//       }
+//     }
+//   });
+// });
+
+
+function initInnerSwipers() {
+  var innerSwiperContainers = document.querySelectorAll('.inner-swiper-container');
+
+  innerSwiperContainers.forEach(function (innerSwiperContainer) {
+    var innerSwiper = new Swiper(innerSwiperContainer, {
+      direction: 'vertical',
+      // other configuration options for the inner swiper
+      spaceBetween: 10, // Adjust the spacing between slides
+      slidesPerView: 1, // Set the number of visible slides to 1
+      mousewheel: {
+        enabled: true
+      },
+      on: {
+        init: function () {
+          handleSlideChange(innerSwiperContainer);
+        },
+        slideChange: function () {
+          handleSlideChange(innerSwiperContainer);
+        }
+      }
+    });
+
+    var dotButtons = innerSwiperContainer.querySelectorAll('.btn-dot');
+
+    dotButtons.forEach(function (button, index) {
+      button.addEventListener('click', function () {
+        innerSwiper.slideTo(index);
+      });
+    });
+  });
+}
+
+function handleSlideChange(innerSwiperContainer) {
+  var activeIndex = innerSwiperContainer.swiper.activeIndex;
+  var dotButtons = innerSwiperContainer.parentElement.parentElement.querySelectorAll('.btn-dot');
+
+  dotButtons.forEach(function (button, index) {
+    if (index === activeIndex) {
+      button.classList.add('active');
+    } else {
+      button.classList.remove('active');
+    }
+  });
+}
+
+initInnerSwipers();
