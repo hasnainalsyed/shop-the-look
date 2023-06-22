e019222516bc2d0680729d447d6f3177f2df7087
Hasnain Syed <hasnainalsyed@gmail.com>
2023-06-22 19:58:15 +0500

    Mobile Popups are Opening

--- backup.js
+++ backup.js
@@ -9,37 +9,6 @@ var swiper = new Swiper('.main-swiper-container', {
   }
 });
 
-// var innerSwipers = document.querySelectorAll('.inner-swiper-container');
-// innerSwipers.forEach(function (innerSwiperContainer) {
-//   console.log(innerSwiperContainer);
-//   var innerSwiper = new Swiper(innerSwiperContainer, {
-//     direction: 'vertical',
-//     // other configuration options for the inner swiper
-//     spaceBetween: 10, // Adjust the spacing between slides
-//     slidesPerView: 1, // Set the number of visible slides to 1
-//     mousewheel: {
-//       enabled: true
-//     },
-//     on: {
-//       slideChange: function () {
-//         var activeIndex = this.activeIndex;
-//         var innerSwiperContainerParent = innerSwiperContainer.parentElement.parentElement;
-//         var dotButtons = innerSwiperContainerParent.querySelectorAll('.btn-dot');
-
-//         dotButtons.forEach(function (button, index) {
-//           console.log(button, index);
-//           if (index === activeIndex) {
-//             button.classList.add('active');
-//           } else {
-//             button.classList.remove('active');
-//           }
-//         });
-//       }
-//     }
-//   });
-// });
-
-
 function initInnerSwipers() {
   var innerSwiperContainers = document.querySelectorAll('.inner-swiper-container');
 
@@ -62,7 +31,7 @@ function initInnerSwipers() {
       }
     });
 
-    var dotButtons = innerSwiperContainer.querySelectorAll('.btn-dot');
+    var dotButtons = innerSwiperContainer.parentElement.parentElement.querySelectorAll('.btn-dot');
 
     dotButtons.forEach(function (button, index) {
       button.addEventListener('click', function () {
@@ -86,3 +55,46 @@ function handleSlideChange(innerSwiperContainer) {
 }
 
 initInnerSwipers();
+
+
+const popupSlides = document.querySelectorAll('.swiper-slide.popup');
+const rows = document.querySelectorAll('.swiper-slide .row');
+let cols = [];
+let screenWidth = window.innerWidth || document.documentElement.clientWidth;
+
+function handleScreenSize(screenWidth) {
+  if (screenWidth >= 991) {
+    popupSlides.forEach(function (slide) {
+      slide.remove();
+    });
+
+    for (let i = 0; i < cols.length; i++) {
+      let col = cols[i];
+      let row = rows[i];
+      row.appendChild(col);
+    }
+  } else {
+    popupSlides.forEach(function (slide) {
+      document.querySelector('.container').appendChild(slide);
+    });
+
+    for (let i = 0; i < cols.length; i++) {
+      let col = cols[i];
+      col.remove();
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  rows.forEach(row => {
+    let col = row.querySelector('.main-inner-swiper-container');
+    cols.push(col);
+  });
+
+  handleScreenSize(screenWidth);
+
+  window.addEventListener('resize', function () {
+    screenWidth = window.innerWidth || document.documentElement.clientWidth;
+    handleScreenSize(screenWidth);
+  });
+});
--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
+let screenWidth = window.innerWidth || document.documentElement.clientWidth;
 // Initialize Swiper with vertical slider
-var swiper = new Swiper('.main-swiper-container', {
+let swiper = new Swiper('.main-swiper-container', {
   slidesPerView: 1, // Set the number of visible slides to 1
   spaceBetween: 10, // Adjust the spacing between slides
   // Configuration options for the main slider
@@ -10,10 +11,10 @@ var swiper = new Swiper('.main-swiper-container', {
 });
 
 function initInnerSwipers() {
-  var innerSwiperContainers = document.querySelectorAll('.inner-swiper-container');
+  const innerSwiperContainers = document.querySelectorAll('.inner-swiper-container');
 
   innerSwiperContainers.forEach(function (innerSwiperContainer) {
-    var innerSwiper = new Swiper(innerSwiperContainer, {
+    let innerSwiper = new Swiper(innerSwiperContainer, {
       direction: 'vertical',
       // other configuration options for the inner swiper
       spaceBetween: 10, // Adjust the spacing between slides
@@ -31,7 +32,7 @@ function initInnerSwipers() {
       }
     });
 
-    var dotButtons = innerSwiperContainer.parentElement.parentElement.querySelectorAll('.btn-dot');
+    const dotButtons = innerSwiperContainer.parentElement.parentElement.querySelectorAll('.btn-dot');
 
     dotButtons.forEach(function (button, index) {
       button.addEventListener('click', function () {
@@ -42,9 +43,9 @@ function initInnerSwipers() {
 }
 
 function handleSlideChange(innerSwiperContainer) {
-  var activeIndex = innerSwiperContainer.swiper.activeIndex;
-  var dotButtons = innerSwiperContainer.parentElement.parentElement.querySelectorAll('.btn-dot');
-
+  const activeIndex = innerSwiperContainer.swiper.activeIndex;
+  const dotButtons = innerSwiperContainer.parentElement.parentElement.querySelectorAll('.btn-dot');
+  // Code for larger screens (>= 991px)
   dotButtons.forEach(function (button, index) {
     if (index === activeIndex) {
       button.classList.add('active');
@@ -55,3 +56,47 @@ function handleSlideChange(innerSwiperContainer) {
 }
 
 initInnerSwipers();
+
+
+const popupSlides = document.querySelectorAll('.swiper-slide.popup');
+const popupSwiperContainers = document.querySelectorAll('.popup-swiper-container');
+const rows = document.querySelectorAll('.swiper-slide .row');
+let cols = [];
+
+
+function handleScreenSize(screenWidth) {
+  if (screenWidth >= 991) {
+    popupSlides.forEach(function (slide) {
+      slide.remove();
+    });
+
+    for (let i = 0; i < cols.length; i++) {
+      let col = cols[i];
+      let row = rows[i];
+      row.appendChild(col);
+    }
+  } else {
+    popupSlides.forEach(function (slide) {
+      document.querySelector('.container').appendChild(slide);
+    });
+
+    for (let i = 0; i < cols.length; i++) {
+      let col = cols[i];
+      col.remove();
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  rows.forEach(row => {
+    let col = row.querySelector('.main-inner-swiper-container');
+    cols.push(col);
+  });
+
+  handleScreenSize(screenWidth);
+
+  window.addEventListener('resize', function () {
+    screenWidth = window.innerWidth || document.documentElement.clientWidth;
+    handleScreenSize(screenWidth);
+  });
+});
